c491b131877daaef37ed5add9827ce8b08179551
Priyagoyal19 <144577998+Priyagoyal19@users.noreply.github.com>
2024-09-29 19:34:38 +0530

    Add files via upload

---
+++ DAY-2 Easy SUBSCRIBE_.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    	Scanner sc = new Scanner(System.in);
+	    int t=sc.nextInt();
+	    
+	for(int i=1;i<=t;i++){
+	    int n=sc.nextInt(),x=sc.nextInt();
+	      int s = (int) Math.ceil((double) n/ 6);
+	      System.out.println(s*x);
+	    
+	}
+
+	}
+}
