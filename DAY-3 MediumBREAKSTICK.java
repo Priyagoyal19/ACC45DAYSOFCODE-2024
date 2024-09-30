6558fc0567411e5df198d66078901864506443d9
Priyagoyal19 <144577998+Priyagoyal19@users.noreply.github.com>
2024-09-30 10:47:28 +0530

    Add files via upload

---
+++ DAY-3 MediumBREAKSTICK.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t =sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int n = sc.nextInt();
+		    int x = sc.nextInt();
+		    
+		    if(n%2==0)
+		           System.out.println("yes");
+		    else if(x%2==0)
+		           System.out.println("no");
+		    else
+		           System.out.println("yes");
+           
+        }
+    }
+}
+
+	
