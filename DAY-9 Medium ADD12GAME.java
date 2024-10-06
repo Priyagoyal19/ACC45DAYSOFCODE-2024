1ad6bd2392ca594c170a562251dbd1d9a61b645c
Priyagoyal19 <144577998+Priyagoyal19@users.noreply.github.com>
2024-10-06 11:10:09 +0530

    Add files via upload

---
+++ DAY-9 Medium ADD12GAME.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+		  
+		     int n= sc.nextInt();
+
+              if(n==1)
+		    {
+		        System.out.println("ALICE");
+		    }
+		    else{
+		        System.out.println("BOB");
+		        
+		    }
+		}
+	}
+}
