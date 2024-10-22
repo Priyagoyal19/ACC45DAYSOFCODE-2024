0e827a886d3253d6eb433a3d0153244eae250ebc
Priyagoyal19 <144577998+Priyagoyal19@users.noreply.github.com>
2024-10-22 19:57:52 +0530

    Add files via upload

---
+++ DAY-20 Easy CHEFGAMES.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+		    int r1=sc.nextInt();
+		    int r2=sc.nextInt();
+		    int r3=sc.nextInt();
+		    int r4=sc.nextInt();
+		    
+		    
+		    
+		    if(r1==1||r2==1||r3==1||r4==1)
+		    {
+		        System.out.println("out");
+		    }
+		      else
+		    {
+		        System.out.println("in");
+		    }
+		}
+
+	}
+}
