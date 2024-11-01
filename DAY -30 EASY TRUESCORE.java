0f9c62a75bf796da56ec957733e09907627529b2
Priyagoyal19 <144577998+Priyagoyal19@users.noreply.github.com>
2024-11-01 09:24:21 +0530

    Add files via upload

---
+++ DAY -30 EASY TRUESCORE.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc = new Scanner(System.in);
+	int t = sc.nextInt();
+	while(t-->0)
+	{
+	    int a = sc.nextInt();
+	     int b = sc.nextInt();
+	      int c = sc.nextInt();
+	       int d = sc.nextInt();
+	       
+	       
+	       if(c<a || d<b)
+	       {
+	           System.out.println("Impossible");
+	       }
+	       else
+	       {
+	            System.out.println("possible");
+	       }
+	}
+
+	}
+}
